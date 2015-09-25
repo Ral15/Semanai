cb931f7ea27c674266992f23fd0a157e2590051c
erikiado <erikiado@Unknown_5c-93-a2-c0-29-69.gateway.2wire.net>
2015-09-25 05:32:51 -0500

    Terminada pagina semanai - Revisar

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-var chartData = [{
+/*var chartData = [{
         "state": "MX-AGU",
         "value": 1
     }, {
@@ -135,10 +135,10 @@ AmCharts.ready(function() {
 
     map.areasSettings = {
         autoZoom: true,
-        color: "#CCC",
+        color: "#FFF",
         selectedColor: "#00992F"
     };
-    console.log(map.allLabels)
+    //console.log(map.allLabels)
     /*map.addListener("clickMapObject", function (event) {
      if (event.mapObject.id != undefined && chartData[event.mapObject.id] != undefined) {
        chart.dataProvider = chartData[event.mapObject.id];
@@ -146,10 +146,131 @@ AmCharts.ready(function() {
        chart.addLabel("0", "!20", event.mapObject.title, "center", 16);
        chart.validateData();
      }
-    });*/
+    });
 
     //map.smallMap = new AmCharts.SmallMap();
 
     map.write("mapspace");
 
 });
+*/
+
+var map = AmCharts.makeChart( "mapspace", {
+  type: "map",
+  "theme": "light",
+
+  colorSteps: 32,
+  dataProvider: {
+    map: "mexicoHigh",
+    areas: [ {
+        "id": "MX-AGU",
+        "value": 9
+    }, {
+        "id": "MX-BCN",
+        "value": 8
+    }, {
+        "id": "MX-BCS",
+        "value": 10
+    }, {
+        "id": "MX-CAM",
+        "value": 15
+    }, {
+        "id": "MX-CHP",
+        "value": 32
+    }, {
+        "id": "MX-CHH",
+        "valie": 6
+    }, {
+        "id": "MX-COA",
+        "value": 5
+    }, {
+        "id": "MX-COL",
+        "value": 4
+    }, {
+        "id": "MX-DIF",
+        "value": 3
+    }, {
+        "id": "MX-DUR",
+        "value": 20
+    }, {
+        "id": "MX-GUA",
+        "value": 16
+    }, {
+        "id": "MX-GRO",
+        "value": 31
+    }, {
+        "id": "MX-HID",
+        "value": 25
+    }, {
+        "id": "MX-JAL",
+        "value": 18
+    }, {
+        "id": "MX-MEX",
+        "value": 13
+    }, {
+        "id": "MX-MIC",
+        "value": 29
+    }, {
+        "id": "MX-MOR",
+        "value": 17
+    }, {
+        "id": "MX-NAY",
+        "value": 22
+    }, {
+        "id": "MX-NLE",
+        "value": 1
+    }, {
+        "id": "MX-OAX",
+        "value": 30
+    }, {
+        "id": "MX-PUE",
+        "value": 24
+    }, {
+        "id": "MX-QUE",
+        "value": 19
+    }, {
+        "id": "MX-ROO",
+        "value": 7
+    }, {
+        "id": "MX-SLP",
+        "value": 23
+    }, {
+        "id": "MX-SIN",
+        "value": 14
+    }, {
+        "id": "MX-SON",
+        "value": 2
+    }, {
+        "id": "MX-TAB",
+        "value": 27
+    }, {
+        "id": "MX-TAM",
+        "value": 11
+    }, {
+        "id": "MX-TLA",
+        "value": 21
+    }, {
+        "id": "MX-VER",
+        "value": 28
+    }, {
+        "id": "MX-YUC",
+        "value": 12
+    }, {
+        "id": "MX-ZAC",
+        "value": 26
+    }]
+    },
+    areasSettings:{
+        autoZoom:true
+    },
+    valueLegend:{
+         right: 5,
+        minValue: "Feliz",
+      maxValue: "Menos Feliz"
+    },
+
+     "export": {
+    "enabled": true
+  }
+
+  } );
